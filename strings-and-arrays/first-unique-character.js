651194949dee3a3856b62dc6627a3ca8ea54b8d7
Ian Turner <ianturner88@gmail.com>
2020-06-27 15:59:16 -0400

    first commit

---
+++ strings-and-arrays/first-unique-character.js
@@ -0,0 +1,23 @@
+var firstUniqueChar = function(s) {
+    // s is the input string 
+
+    // create the hash table
+    const hashTable = {};
+
+    for (let char of s) {
+        // increment the char counter if the char is already stored in the hash table
+        if (hashTable[char]) hashTable[char]++;
+        // initialize the char counter in the hash table to 1
+        else hashTable[char] = 1;
+    }
+
+    for (let i = 0; i < s.length; i++) {
+        // cycle through all individual letters in the string
+        const char = s[i];
+        // check if the character in question has only 1 instance of occurence 
+        if (hashTable[char] == 1) return i;
+    }
+
+    // return if there are no non-repeating characters
+    return -1;
+}
