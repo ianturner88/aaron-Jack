1e59dd05178cf5b4c20b9dd3c326718ec2cbc29b
Ian Turner <ianturner88@gmail.com>
2020-08-13 22:06:47 -0400

    bedtime

--- myAttempts/leetcode20.js
+++ myAttempts/leetcode20.js
@@ -7,12 +7,17 @@
 const isValid = function(s) {
   /* checks if an input has valid parentheses */
 
+  if (s.length % 2 === 1) {
+    // the string doesn't have a matching number of brackets
+    return false;
+  }
+
   // where the string's opening brackets are stored
   const stack = [];
   const brackets = ['(', '[', '{', ')', ']', '}'];
   const isOpening = false;
   let indexValue = 0;
-  const isMatch = false;
+  let isMatch = false;
   let topOfStack;
 
   for (const character in s) {
@@ -20,18 +25,29 @@ const isValid = function(s) {
       // store the bracket's index value in the array
       if (brackets[i] === s[character]) {
         indexValue = i;
+        isMatch = true;
       }
     }
 
     if (indexValue < 3) {
+      // store the number on the stack
       stack.push(brackets[indexValue]);
     } else {
+      // retrieve the top item from the stack
       topOfStack = stack.pop();
       if (topOfStack !== indexValue - 3) {
+        // check if the opening & closing brackets match
         return false;
       }
     }
   }
+
+  if (stack.length > 0) {
+    // there are unmatched brackets remaining
+    return false;
+  }
+
+  return true;
 };
 
-console.log(isValid(')'));
+console.log(isValid('[()]'));
