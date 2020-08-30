48628b234b1f9008d7528d84a3bbda95cac2813a
Ian Turner <ianturner88@gmail.com>
2020-08-30 18:04:55 -0400

    #35 binary search implemented

--- myAttempts/leetcode35.js
+++ myAttempts/leetcode35.js
@@ -1,53 +1,22 @@
 const searchInsert = function (nums, target) {
   let low = 0;
-  let middle;
   let high = nums.length - 1;
 
-  if (nums.length === 1) {
-    if (nums[0] < target) {
-      return 1;
-    }
-    return 0;
-  }
+  while (low <= high) {
+    const mid = Math.floor((high + low) / 2);
+    const midVal = nums[mid];
 
-  if (nums.length === 2) {
-    if (nums[0] > target || nums[0] === target) {
-      return 0;
+    if (target === midVal) {
+      return mid;
     }
-    if ((nums[0] < target && nums[1] > target) || nums[1] === target) {
-      return 1;
-    }
-    if (nums[1] < target) {
-      return 2;
+    if (target > midVal) {
+      low = mid + 1;
+    } else {
+      high = mid - 1;
     }
   }
-
-  while (low !== high) {
-    middle = Math.floor(high / 2);
-
-    if (target <= nums[low]) {
-      // the target is the smallest number
-      return low;
-    }
-    if (target >= nums[high]) {
-      // the target is the largest number
-      return high + 1;
-    }
-    if (target === nums[middle]) {
-      // the target is the middle number
-      return middle;
-    }
-    if (target > nums[middle]) {
-      low = middle + 1;
-    } else if (target < nums[middle]) {
-      high = middle - 1;
-    }
-  }
-
-  console.log(middle);
-
-  return middle;
+  return low;
 };
 
-searchInsert([1, 3, 5], 4);
+searchInsert([1, 3, 5, 7], 9);
 module.exports = searchInsert;
