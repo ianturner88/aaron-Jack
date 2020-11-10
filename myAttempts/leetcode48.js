107ce7fe2adf1783e37c5e07692d21e701250923
Ian Turner <ianturner88@gmail.com>
2020-11-10 09:54:07 -0500

    #48 algorithm solved on paper

---
+++ myAttempts/leetcode48.js
@@ -0,0 +1,18 @@
+const rotate = function (matrix) {
+  // determine matrix dimensions
+  const matrixLength = matrix[0].length;
+  console.log(`Matrix length: ${matrixLength}`);
+
+  // the array to manipulate
+  matrixAnswer = matrix;
+};
+
+matrix = [
+  [21, 22, 23, 24, 25],
+  [20, 7, 8, 9, 10],
+  [19, 6, 1, 2, 11],
+  [18, 5, 4, 3, 12],
+  [17, 16, 15, 14, 13],
+];
+
+rotate(matrix);
