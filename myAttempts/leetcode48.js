c9d7655583cc142da4d8e31ea185142fe4087697
Ian Turner <ianturner88@gmail.com>
2020-11-12 09:51:25 -0500

    flipping the matrix along the diagonal is proving tricky

--- myAttempts/leetcode48.js
+++ myAttempts/leetcode48.js
@@ -1,32 +1,47 @@
+/* eslint-disable no-plusplus */
+const matrix = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
+
+const matrix1 = [
+  [5, 1, 9, 11],
+  [2, 4, 8, 10],
+  [13, 3, 6, 7],
+  [15, 14, 12, 16],
+];
+
 const rotate = function (matrix) {
-  // determine matrix dimensions
-  const matrixLength = matrix[0].length;
   // the array to manipulate
   const matrixAnswer = matrix;
 
+  // boundaries
   let countTop = 0;
-  let countBottom = matrix.length;
+  let countBottom = matrix.length - 1;
+  const matrixLength = Math.round(matrix.length / 2);
 
-  console.log(`hello${matrixAnswer}`);
-
-  while (countTop != matrix.length / 2) {
+  while (countTop !== matrixLength) {
+    // flip the matrix
+    const tempArray = matrixAnswer[countTop];
     matrixAnswer[countTop] = matrix[countBottom];
+    matrixAnswer[countBottom] = tempArray;
 
     countTop++;
     countBottom--;
   }
 
-  console.log(`hello${matrixAnswer}`);
+  for (let x = 0; x < matrixLength; x++) {
+    for (let y = 1; y < matrixLength; y++) {
+      const temp = matrixAnswer[x + 1][y - 1];
+      matrixAnswer[x + 1][y - 1] = matrixAnswer[y][x];
+      matrixAnswer[y][x] = temp;
+    }
+  }
+
+  console.log(matrixAnswer);
 };
 
-matrix = [
-  [21, 22, 23, 24, 25],
-  [20, 7, 8, 9, 10],
-  [19, 6, 1, 2, 11],
-  [18, 5, 4, 3, 12],
-  [17, 16, 15, 14, 13],
-];
-
-console.log('hello');
-
-// rotate(matrix);
+rotate(matrix);
+console.log('Hello');
+rotate(matrix1);
