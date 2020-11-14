fc0f83f170da6a6cd9a0c06d2898d85e734649c6
Ian Turner <ianturner88@gmail.com>
2020-11-14 10:29:38 -0500

    #48 logic explanation written out

--- myAttempts/leetcode48.js
+++ myAttempts/leetcode48.js
@@ -1,5 +1,26 @@
 /* eslint-disable no-plusplus */
 /* eslint-disable camelcase */
+
+/*
+input:
+[
+[1,2,3],
+[4,5,6],
+[7,8,9]
+]
+
+flip the rows -> row 0 becomes row nth (and vice verse)
+so on and so on
+[
+[7,8,9],
+[4,5,6],
+[1,2,3]
+]
+
+swap numbers along the diagonal
+- switch 4 with 8, 1 with 9...
+*/
+
 const rotate = function (matrix) {
   // the array to manipulate
   const matrixAnswer = matrix;
@@ -22,6 +43,7 @@ const rotate = function (matrix) {
   let diagonal_Counter = 1;
 
   for (let x = 1; x <= matrixAnswer.length - 1; x++) {
+    /* flip the numbers along the diagonal */
     diagonal_Counter++;
     for (let y = 0; y < diagonal_Counter - 1; y++) {
       const temp = matrixAnswer[y][x];
