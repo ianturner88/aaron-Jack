bf73b553934f1f2e0897a2f340027189a172fa28
Ian Turner <ianturner88@gmail.com>
2020-11-14 09:48:48 -0500

    Leetcode 48 complete

--- myAttempts/leetcode48.js
+++ myAttempts/leetcode48.js
@@ -1,17 +1,5 @@
 /* eslint-disable no-plusplus */
-const matrix = [
-  [1, 2, 3],
-  [4, 5, 6],
-  [7, 8, 9],
-];
-
-const matrix1 = [
-  [5, 1, 9, 11],
-  [2, 4, 8, 10],
-  [13, 3, 6, 7],
-  [15, 14, 12, 16],
-];
-
+/* eslint-disable camelcase */
 const rotate = function (matrix) {
   // the array to manipulate
   const matrixAnswer = matrix;
@@ -31,17 +19,16 @@ const rotate = function (matrix) {
     countBottom--;
   }
 
-  for (let x = 0; x < matrixLength; x++) {
-    for (let y = 1; y < matrixLength; y++) {
-      const temp = matrixAnswer[x + 1][y - 1];
-      matrixAnswer[x + 1][y - 1] = matrixAnswer[y][x];
-      matrixAnswer[y][x] = temp;
+  let diagonal_Counter = 1;
+
+  for (let x = 1; x <= matrixAnswer.length - 1; x++) {
+    diagonal_Counter++;
+    for (let y = 0; y < diagonal_Counter - 1; y++) {
+      const temp = matrixAnswer[y][x];
+      matrixAnswer[y][x] = matrixAnswer[x][y];
+      matrixAnswer[x][y] = temp;
     }
   }
 
-  console.log(matrixAnswer);
+  return matrixAnswer;
 };
-
-rotate(matrix);
-console.log('Hello');
-rotate(matrix1);
